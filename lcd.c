a11616bce171515f8418ea509affbd44bdca0d7f
rehamashrafshouman <rehamshouman2015@gmail.com>
2019-05-13 18:12:55 +0300

    Create LCD part 2 functions

---
+++ lcd.c
@@ -0,0 +1,40 @@
+void LCD_init(void)
+{
+	SYSCTL_RCGCGPIO_R = 0x03;
+	while((SYSCTL_PRGPIO_R&0x03)==0);
+	GPIO_PORTB_DIR_R = 0xFF;
+	GPIO_PORTB_DEN_R = 0xFF;
+	GPIO_PORTA_DIR_R = 0xE0;
+	GPIO_PORTA_DEN_R = 0xE0;
+	
+	LCD_COMMAND (0x38);
+	sysTick_wait10ms(1);
+	LCD_COMMAND (0x0C);
+	sysTick_wait10ms(1);
+		LCD_COMMAND (0x01);
+	sysTick_wait10ms(1);
+		LCD_COMMAND (0x06);
+	sysTick_wait10ms(1);
+
+}
+
+void LCD_COMMAND (char com)
+{
+	GPIO_PORTA_DATA_R = 0;
+	sysTick_wait10ms(1);
+	GPIO_PORTA_DATA_R|=0x80;
+	sysTick_wait10ms(1);
+	GPIO_PORTB_DATA_R = com;
+	sysTick_wait10ms(1);
+	GPIO_PORTA_DATA_R=0;
+	sysTick_wait10ms(1);
+}
+
+void LCD_DATA_STRING (char s[])
+{
+	int i=0;
+	while(s[i]!= '\0') // check for null character
+	{
+		LCD_DATA_CHAR(s[i++]);
+	}
+}
---
+++ lcd.h
@@ -0,0 +1,6 @@
+#ifndef LCD_H
+#define LCD_H
+void LCD_init(void);
+void LCD_COMMAND (char com);
+void LCD_DATA_STRING (char s[]);
+#endif
