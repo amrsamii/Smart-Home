6c70cf3ba2d4012f8b3ac1806f57ed233010c98d
Solariii <Solariii>
2019-05-13 16:34:29 +0200

    create pwm module for controlling intensity of led

---
+++ micro_config.h
@@ -0,0 +1,20 @@
+ /******************************************************************************
+ *
+ * Module: Micro - Configuration
+ *
+ * File Name: Micro_Config.h
+ *
+ * Description: File include all Microcontroller libraries
+ *
+ * Author: Eslam Tarek
+ *
+ *******************************************************************************/
+
+#ifndef MICRO_CONFIG_H_
+#define MICRO_CONFIG_H_
+
+
+#include"stdint.h"
+#include"K:/keil/TExaSware/tm4c123gh6pm.h"
+
+#endif /* MICRO_CONFIG_H_ */
