31d3ab0f45072b783cccd5e3aeb4a7266421c6a4
Solariii <mennaayman618@gmail.com>
2019-05-13 17:06:34 +0200

    create uart recieve module

---
+++ menna/uart.c
@@ -0,0 +1,25 @@
+#include"uart.h"
+
+
+uint8_t UART0_Available(void){
+	return ((UART0_FR_R&UART_FR_RXFE) == UART_FR_RXFE) ? 0 : 1;
+}
+
+uint8_t UART0_Read(void){
+	while(UART0_Available() != 1);
+	return (uint8_t)(UART0_DR_R&0xFF);
+}
+
+
+void UART_receiveString(uint8_t *Str)
+
+{
+	uint8_t i = 0;
+	Str[i] = UART0_Read();
+	while(Str[i] != '#')
+	{
+		i++;
+		Str[i] = UART0_Read();
+	}
+	Str[i] = '\0';
+}
---
+++ menna/uart.h
@@ -0,0 +1,20 @@
+#ifndef __UART_H__
+#define	__UART_H__
+
+#include "micro_config.h"
+
+#define GPIO_PA10_M 0x03
+void UART0_Init(void);
+
+uint8_t UART0_Available(void);
+
+uint8_t UART0_Read(void);
+
+void UART0_Write(uint8_t data);
+
+void UART_sendString(const uint8_t *Str);
+
+void UART_receiveString(uint8_t *Str);
+
+
+#endif // __UART_H__
