1b54391db72ac8c239a2857f9fc202956f1981f0
mennaAyman <mennaayman618@gmail.com>
2019-05-15 14:03:44 +0200

    generic uart module

--- uart.h
+++ uart.h
@@ -1,16 +1,13 @@
 #ifndef __UART_H__
 #define	__UART_H__
-#define GPIO_PA10_M 0x03
 
 #include "micro_config.h"
-void UART0_Init(void);
+void UART_Init(uint8_t index);
 
-uint8_t UART0_Available(void);
+uint8_t UART_Available(uint8_t index);
 
-uint8_t UART0_Read(void);
-
-void UART0_Write(uint8_t data);
-
+uint8_t UART_Read(uint8_t index);
+void UART_Write(uint8_t index,uint8_t data);
 void UART_sendString(const uint8_t *Str);
 
 void UART_receiveString(uint8_t *Str);
