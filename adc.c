0aec9de5b52c5e684cba4337d7ecd74c9bff05ad
amrsamii <amr_samy12@yahoo.com>
2019-05-16 14:39:13 +0200

    Generalize ADC for all channels

--- adc.c
+++ adc.c
@@ -1,27 +1,165 @@
-#include"adc.h"
+#include "adc.h"
 
 
-void ADC0_Init(void){
-	SYSCTL_RCGCADC_R|=  ADC0_CLK_M;
-	SYSCTL_RCGCGPIO_R|= GPIO_E_M;
-	ADC0_ACTSS_R&=~(ADC0_ACTSS_SS3);
-	ADC0_EMUX_R&=~(ADC0_SS3_TRIG);
-	ADC0_SSMUX3_R = ADC0_SSMUX3;
-	ADC0_SSCTL3_R |= (ADC0_SSCTL3_IEO|ADC0_SSCTL3_END0);
-	ADC0_ACTSS_R|=ADC0_ACTSS_SS3;
-GPIO_PORTE_AFSEL_R|= GPIO_PE2_M;
-	GPIO_PORTE_DEN_R &=~(GPIO_PE2_M);
-	GPIO_PORTE_AMSEL_R |= GPIO_PE2_M;
-	GPIO_PORTE_DIR_R&=~(GPIO_PE2_M);
+
+//ADC Initilization
+
+void ADC0_Init(uint8_t channel)
+{
+   
+    SYSCTL_RCGCADC_R |= SYSCTL_RCGCADC_R0;
+
+    switch (channel)
+    {
+    case AIN0:
+        
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
+        //Delay for the clock
+        channel = 0;
+        
+        GPIO_PORTE_AFSEL_R |= PIN3;
+				GPIO_PORTE_DEN_R &= ~(PIN3);
+        GPIO_PORTE_AMSEL_R |= PIN3;
+        break;
+
+    case AIN1:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
+        channel = 0;
+        GPIO_PORTE_AFSEL_R |= PIN2;
+        GPIO_PORTE_DEN_R &= ~(PIN2);
+        GPIO_PORTE_AMSEL_R |= PIN2;
+        break;
+
+    case AIN2:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
+        channel = 0;
+        GPIO_PORTE_AFSEL_R |= PIN1;
+        GPIO_PORTE_DEN_R &= ~(PIN1);
+        GPIO_PORTE_AMSEL_R |= PIN1;
+        break;
+
+    case AIN3:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
+        channel = 0;
+        GPIO_PORTE_AFSEL_R |= PIN0;
+        GPIO_PORTE_DEN_R &= ~(PIN0);
+        GPIO_PORTE_AMSEL_R |= PIN0;
+        break;
+
+    case AIN4:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;
+        channel = 0;
+        GPIO_PORTD_AFSEL_R |= PIN3;
+        GPIO_PORTD_DEN_R &= ~(PIN3);
+        GPIO_PORTD_AMSEL_R |= PIN3;
+        break;
+
+    case AIN5:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;
+        channel = 0;
+        GPIO_PORTD_AFSEL_R |= PIN2;
+        GPIO_PORTD_DEN_R &= ~(PIN2);
+        GPIO_PORTD_AMSEL_R |= PIN2;
+        break;
+
+    case AIN6:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;
+        channel = 0;
+        GPIO_PORTD_AFSEL_R |= PIN1;
+        GPIO_PORTD_DEN_R &= ~(PIN1);
+        GPIO_PORTD_AMSEL_R |= PIN1;
+        break;
+
+    case AIN7:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;
+        channel = 0;
+        GPIO_PORTD_AFSEL_R |= PIN0;
+        GPIO_PORTD_DEN_R &= ~(PIN0);
+        GPIO_PORTD_AMSEL_R |= PIN0;
+        break;
+
+    case AIN8:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
+        channel = 0;
+        GPIO_PORTE_AFSEL_R |= PIN5;
+        GPIO_PORTE_DEN_R &= ~(PIN5);
+        GPIO_PORTE_AMSEL_R |= PIN5;
+		    GPIO_PORTE_DIR_R &= ~(PIN5);
+        break;
+
+    case AIN9:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
+        channel = 0;
+        GPIO_PORTE_AFSEL_R |= PIN4;
+        GPIO_PORTE_DEN_R &= ~(PIN4);
+        GPIO_PORTE_AMSEL_R |= PIN4;
+        break;
+
+    case AIN10:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;
+        channel = 0;
+        GPIO_PORTB_AFSEL_R |= PIN5;
+        GPIO_PORTB_DEN_R &= ~(PIN5);
+        GPIO_PORTB_AMSEL_R |= PIN5;
+        break;
+
+    case AIN11:
+        SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;
+        channel = 0;
+        GPIO_PORTB_AFSEL_R |= PIN4;
+        GPIO_PORTB_DEN_R &= ~(PIN4);
+        GPIO_PORTB_AMSEL_R |= PIN4;
+        break;
+    }
+    return;
 }
 
-uint16_t ADC_SS3_In(){
-	
-	uint16_t data;
-	ADC0_PSSI_R = ACD0_PSSI_SS3;
-	while((ADC0_RIS_R & ADC0_RIS_INR3 )!=ADC0_RIS_INR3);
-	data = ADC0_SSFIFO3_R & 0xFFF;
-	
-	ADC0_ISC_R = ADC0_RIS_INR3;
-	return data;
+//Sample Sequencer Initilization for ADC0 Sample Sequencer 3
+
+void ADC0_SS3_Init(uint8_t channel, uint16_t mode, uint8_t temp_en)
+{
+    //Ensure ADC is not busy
+    while (ADC0_ACTSS_R & ADC_ACTSS_BUSY)
+        ;
+    ADC0_ACTSS_R &= ~ADC_ACTSS_ASEN3;
+    
+    ADC0_EMUX_R &= ~ADC_EMUX_EM3_ALWAYS;
+    ADC0_EMUX_R |= mode;
+   
+
+    ADC0_SSMUX3_R |= channel;
+
+    
+    if (temp_en == ENABLE)
+    {
+        ADC0_SSCTL3_R |= ADC_SSCTL3_TS0;
+    }
+    else
+    {
+        ADC0_SSCTL3_R &= ~(ADC_SSCTL3_TS0);
+    }
+    ADC0_SSCTL3_R |= ADC_SSCTL3_IE0;
+    ADC0_SSCTL3_R |= ADC_SSCTL3_END0;
+    ADC0_SSCTL3_R &= ~(ADC_SSCTL3_D0);
+
+   
+    ADC0_IM_R &= ~(ADC_IM_MASK3);
+  
+    ADC0_ACTSS_R |= ADC_ACTSS_ASEN3;
+    return;
+}
+
+uint16_t ADC0_SS3_Read()
+{
+	uint16_t result;
+    //This is only if the trigger is default
+    ADC0_PSSI_R |= ADC_PSSI_SS3;
+    //poling on raw interrupt
+    while (!(ADC0_RIS_R & ADC_RIS_INR3))
+        ;
+    result = ADC0_SSFIFO3_R & ADC_SSFIFO3_DATA_M;
+    //Clear flag before returning the value
+    ADC0_ISC_R |= ADC_ISC_IN3;
+    return result;
+
 }
--- adc.h
+++ adc.h
@@ -1,18 +1,36 @@
-#ifndef ADC_H_
-#define ADC_H_
+#ifndef __ADC_H__
+#define __ADC_H__
 
-#include "micro_config.h"
-#define GPIO_E_M 0x10
-#define ADC0_CLK_M 0x01
-#define GPIO_PE2_M  (0x1<<2)
-#define ADC0_ACTSS_SS3 0x8
-#define ADC0_SS3_TRIG 0x0FFF
-#define ADC0_SSMUX3  0x0
-#define ADC0_SSCTL3_IEO (0x1 << 2)
-#define ADC0_SSCTL3_END0 (0x1 << 1)
-#define ACD0_PSSI_SS3 (0x1 <<3)
-#define ADC0_RIS_INR3 (0x1 <<3)
-void ADC0_Init(void);
-uint16_t ADC_SS3_In();
+#include"micro_config.h"
 
-#endif
+#define PIN0 1
+#define PIN1 2
+#define PIN2 4
+#define PIN3 8
+#define PIN4 16
+#define PIN5 32
+#define PIN6 64
+#define PIN7 128
+
+#define AIN0 0   //PE3
+#define AIN1 1   //PE2
+#define AIN2 2   //PE1
+#define AIN3 3   //PE0
+#define AIN4 4   //PD3
+#define AIN5 5   //PD2
+#define AIN6 6   //PD1
+#define AIN7 7   //PD0
+#define AIN8 8   //PE5
+#define AIN9 9   //PE4
+#define AIN10 10 //PB5
+#define AIN11 11 //PB4
+
+#define ENABLE 1
+#define DISABLE 0
+
+//Prototypes
+void ADC0_Init(uint8_t channel);
+void ADC0_SS3_Init(uint8_t channel, uint16_t mode, uint8_t temp_en);
+uint16_t ADC0_SS3_Read(void);
+
+#endif // __ADC_H__
