1fc0d970ebaa1859fbdf09104b9845ca617cfa2a
Solariii <eslamsolari1996@gmail.com>
2019-05-17 03:42:11 +0200

    edit motor functions

--- motor.c
+++ motor.c
@@ -1,16 +1,11 @@
 #include "motor.h"
+#include "systick.h"
 
 
 
 
-#define CLOCK_CYCLES_PER_MS 16000
 
-void systick_init(void){
-	NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE;
-	NVIC_ST_RELOAD_R = NVIC_ST_RELOAD_M;
-	NVIC_ST_CURRENT_R = NVIC_ST_CURRENT_S;
-	NVIC_ST_CTRL_R |= (NVIC_ST_CTRL_CLK_SRC | NVIC_ST_CTRL_ENABLE);
-}
+
 
 
 
@@ -19,48 +14,17 @@ int j;
 	for( j=0 ; j<4 ; j++ )
 	{
 		GPIO_PORTD_DATA_R =0x03;
-		delay(10);
+		_delay_ms(10);
 		GPIO_PORTD_DATA_R =0x6;
-		delay(10);
+		_delay_ms(10);
 		GPIO_PORTD_DATA_R =0x0c;
-		delay(10);
+		_delay_ms(10);
 		GPIO_PORTD_DATA_R =0x09;
-		delay(10);
+		_delay_ms(10);
 		
 	}
 
 }
-
-
-
-void delay(uint32_t time){
-	uint32_t i;
-	for(i = 0;i < time;i++){
-		systick_waitMS();
-	}
-}
-
-
-
-
-
-
-
-void systick_waitMS(void){
-	NVIC_ST_RELOAD_R = CLOCK_CYCLES_PER_MS - 1;
-	NVIC_ST_CURRENT_R = 0;
-	while((NVIC_ST_CTRL_R&NVIC_ST_CTRL_COUNT) != NVIC_ST_CTRL_COUNT);
-}
-
-/*void systick_wait1ms(void){
-	uint32_t elapsedTime;
-  uint32_t startTime = NVIC_ST_CURRENT_R;
-  do{
-    elapsedTime = (startTime - NVIC_ST_CURRENT_R)&0x00FFFFFF;
-  }
-  while(elapsedTime <= CLOCK_CYCLES_PER_MS);
-}*/
-
 
 
 
@@ -80,13 +44,13 @@ void motor_rotateAnticlockwise(void)
 for(i=0;i<4;i++)
 	{
 		GPIO_PORTD_DATA_R =0x09;
-		delay(10);
+		_delay_ms(10);
 		GPIO_PORTD_DATA_R =0xC;
-		delay(10);
+		_delay_ms(10);
 		GPIO_PORTD_DATA_R =0x06;
-		delay(10);
+		_delay_ms(10);
 		GPIO_PORTD_DATA_R =0x03;
-		delay(10);
+		_delay_ms(10);
 		
 	}
 }
